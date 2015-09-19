652cbe1394f3401b0484f1a8945ea89879d9803c
JackQinCh <jackqin3956@gmail.com>
2015-09-19 07:54:18 -0400

    Initial commit

---
+++ app/src/androidTest/java/edu/njit/fall15/team1/cs673messenger/ApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.njit.fall15.team1.cs673messenger;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
